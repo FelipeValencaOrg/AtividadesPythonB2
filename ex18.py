eaac5b297ee36578cca41633a5efe2fda968e87f
unknown <2mtec@e041edu.local>
2026-04-29 07:32:37 -0300

    Exercícios 17 ao 19

---
+++ ex18.py
@@ -0,0 +1,7 @@
+tupla = [
+    "Dahmer",
+    "Valença",
+    "Mendes"
+    ]
+    
+print(tupla[1])
---
+++ ex19.py
@@ -0,0 +1,8 @@
+
+dadosAluno = {
+    "nome": "Felipe Mendes",
+    "idade": 16,
+    "nomeMae": "Marli"
+}
+
+print(dadosAluno)
